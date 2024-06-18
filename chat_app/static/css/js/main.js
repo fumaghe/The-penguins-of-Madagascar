81629abde64b0da4585f639baba0aceb949cfb1b
skad27 <adam.pezz@hotmail.com>
2024-06-18 17:18:20 +0200

    1.2

--- chat_app/static/css/js/main.js
+++ chat_app/static/css/js/main.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Script loaded');
+
+    const loginBtn = document.getElementById('login-btn');
+    loginBtn.addEventListener('click', () => {
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+        // Esegui la logica di login qui...
+        console.log('Username:', username);
+        console.log('Password:', password);
+    });
 });
+
