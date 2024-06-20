98ede63069af63ad740ba76ea3a27d737f782c4d
Andrea Fumagalli <160780597+fumaghe@users.noreply.github.com>
2024-06-20 14:02:15 +0200

    Riproviamo

--- messaging_app/app/utils.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-
-def hash_password(password):
-    return hashlib.sha256(password.encode()).hexdigest()
-
-def verify_password(password, hashed):
-    return hashlib.sha256(password.encode()).hexdigest() == hashed
--- messaging_app/config.py
+++
@@ -1,8 +0,0 @@
-import redis
-
-# Configurazione della connessione a Redis
-REDIS_HOST = 'localhost'
-REDIS_PORT = 6379
-REDIS_DB = 0
-
-redis_client = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
